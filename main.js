99f482dd20514c8fbfdf4b343eb610fc5bfe4779
theone <theone@theone.com>
2021-12-17 15:35:08 +0100

    Some code refactoring. New methods and variables.

--- main.js
+++ main.js
@@ -1,50 +1,87 @@
-let addToDoButton = document.getElementById("addToDoBtn");
-let toDoContainer = document.getElementById("toDoContainer");
-let inputFieldToDO = document.getElementById("inputFieldToDo");
-const storedInput = localStorage.getItem("textInput");
-const clear = document.querySelector(".clear");
+let addToDoButton = document.getElementById('addToDoBtn');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputFieldToDo = document.getElementById('inputFieldToDo');
+const clear = document.querySelector('.clear');
 
-addToDoButton.addEventListener("click", function () {
-  let paragraph = document.createElement("li");
-  paragraph.classList.add("paragraph-styling", "to-dos");
-  paragraph.innerText = inputFieldToDO.value;
-
-  saveToLocalStorage(inputFieldToDO.value);
-
-  toDoContainer.appendChild(paragraph);
-  inputFieldToDO.value = "";
-
-  paragraph.addEventListener("click", function () {
-    paragraph.style.textDecoration = "line-through";
-  });
-  paragraph.addEventListener("dblclick", function () {
-    toDoContainer.removeChild(paragraph);
-  });
-});
-
-//Execute a function when the user releases a key on the keyboard
-inputFieldToDO.addEventListener("keyup", function (event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("addToDoBtn").click();
-  }
-});
-
-const saveToLocalStorage = (newValue) => {
-  localStorage.setItem("textInput", toDoContainer.textContent); //ORRI
-};
-
-addToDoButton.addEventListener("click", saveToLocalStorage);
-
-if (inputFieldToDO) {
-  toDoContainer.textContent = storedInput;
+// this will make the stored-data in localStorage available as a JavaScript-array, instead of LocalStorage-data
+// start the app with checking if the stored-data is empty/null and if so, then create an empty array
+let storedInput = localStorage.getItem('textInput');
+if (storedInput === '' || storedInput === null) {
+    storedInput = [];
+    // push it to localStorage with JSON.stringify()
+    localStorage.setItem('textInput', JSON.stringify(storedInput));
+} else {
+    // else, if its not empty or null, then parse it/make it readable for JavaScript by JSON.parse()
+    storedInput = JSON.parse(localStorage.getItem('textInput'));
 }
 
-// clear the local storage
-clear.addEventListener("click", function () {
-  localStorage.clear();
-  location.reload();
+addToDoButton.addEventListener('click', function () {
+    // RETURN/BREAK if the input field is empty
+    if (inputFieldToDo.value === '') return;
+
+    saveToLocalStorage();
+    displayStoredToDoList();
 });
+
+const removeToDo = (paragraph) => {
+    toDoContainer.removeChild(paragraph);
+
+    // this will return every toDo that is NOT(!==) the same
+    storedInput = storedInput.filter((toDo) => toDo !== paragraph.innerText);
+
+    // then stringify(JSON.stringify()) the new array and store it in localStorage
+    localStorage.setItem('textInput', JSON.stringify(storedInput));
+};
+
+//Execute a function when the user releases a key on the keyboard
+inputFieldToDo.addEventListener('keyup', function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById('addToDoBtn').click();
+    }
+});
+
+const saveToLocalStorage = () => {
+    // 'storedInput' will get/be added a new value by this spread-function
+    // spread-function is like = [all-content-from-an-array, the-new-value-to-be-added]
+    // https://www.samanthaming.com/tidbits/14-combine-multiple-arrays-using-spread/
+    storedInput = [...storedInput, inputFieldToDo.value];
+
+    // then stringify(JSON.stringify()) the new array and store it in localStorage
+    localStorage.setItem('textInput', JSON.stringify(storedInput));
+};
+
+// clear the local storage
+clear.addEventListener('click', function () {
+    localStorage.clear();
+    location.reload();
+});
+
+const displayStoredToDoList = () => {
+    // FIRST remove everything from 'toDoContainer' to rerender it again
+    toDoContainer.innerHTML = '';
+
+    // NEXT, loop through EVERY-SINGLE toDo in the STORED toDo-list AND render/display it
+    for (let toDo of storedInput) {
+        let paragraph = document.createElement('li');
+        paragraph.classList.add('paragraph-styling', 'to-dos');
+        paragraph.innerText = toDo;
+
+        toDoContainer.appendChild(paragraph);
+        inputFieldToDo.value = '';
+
+        paragraph.addEventListener('click', function () {
+            paragraph.style.textDecoration = 'line-through';
+        });
+        paragraph.addEventListener('dblclick', function () {
+            removeToDo(paragraph);
+        });
+    }
+};
+
+// This method will start when everything above is created/stored/done.
+// This method will get the local-stored data and display it
+displayStoredToDoList();
